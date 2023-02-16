9ace3642e38242c26547e86b5812c38764cbef0f
DESKTOP-BFTF0AS\tjoeun <yeomjh2000@gmail.com>
2023-02-16 12:52:05 +0900

    04_MyBatis_chap04_crud

---
+++ src/com/greedy/section03/remix/MenuMapper.java
@@ -0,0 +1,12 @@
+package com.greedy.section03.remix;
+
+import java.util.List;
+
+public interface MenuMapper {
+	
+	List<MenuDTO> selectAllMenu();
+
+	MenuDTO selectMenuByCode(int code);
+
+	int insertMenu(MenuDTO menu);	
+}
